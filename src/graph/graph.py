8eb1791e37ae4139738a9b2a0776d314c388e235
Carlos Eduardo Gracite do Amaral <carlosgracite@gmail.com>
2013-02-06 15:40:51 -0200

    Now the graph supports distance between vertices.

--- src/graph/graph.py
+++ src/graph/graph.py
@@ -1,23 +1,39 @@
 # coding: utf-8
 
-class VerticeNotFoundException(BaseException):
-	pass
-
 
 class Graph(object):
+	"""
+		This implementation actualy doesn't permits:
+			- two or more edges between vertices;
+	"""
 	def __init__(self):
 		self.edges = {}
+		self.distances = {}
 
 	def add_vertice(self, v):
-		if not self.edges.has_key(v):
+		if v not in self.edges:
 			self.edges[v] = []
 
-	def add_edge(self, v1, v2):
+	def add_edge(self, v1, v2, d=1):
 		self.add_vertice(v1)
 		self.add_vertice(v2)
 		self.edges[v1].append(v2)
+		self.distances[(v1, v2)] = d
 
 	def adj(self, v):
 		if v not in self.edges:
 			raise VerticeNotFoundException
 		return self.edges[v]
+
+	def distance(self, v1, v2):
+		if (v1, v2) not in self.distances:
+			raise EdgeNotFoundException
+		return self.distances[(v1, v2)]
+
+
+class VerticeNotFoundException(BaseException):
+	pass
+
+
+class EdgeNotFoundException(BaseException):
+	pass
